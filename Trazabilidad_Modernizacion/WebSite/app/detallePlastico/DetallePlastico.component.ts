01dbeb35cd41251c00ea5dfbadad4a352d913d89
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2020-03-25 11:40:06 -0300

    modificacion del boton de la tabla consultas plasticos

--- Trazabilidad_Modernizacion/WebSite/app/detallePlastico/DetallePlastico.component.ts
+++ Trazabilidad_Modernizacion/WebSite/app/detallePlastico/DetallePlastico.component.ts
@@ -4,6 +4,8 @@ import { DetallePlasticoService } from '../services/detallePlastico.services';
 import { DetallePlasticoModel } from '../model/detallePlasticoModel';
 import { } from '@angular/common/http'
 import { error } from '@angular/compiler/src/util';
+import { Route } from '@angular/compiler/src/core';
+//import { ActivatedRoute, Params } from '@angular/router';
 @Component({
     selector: 'consultas-component',
     templateUrl: 'app/detallePlastico/detallePlastico.component.html' 
@@ -13,15 +15,20 @@ export class DetallePlasticoComponent implements OnInit {
     public idPlastico: string;
     public msg: string;
     constructor(private _DetallePlasticoService: DetallePlasticoService
-)   {
+        //private rutaActiva: ActivatedRoute
+     
+    ) {
+     
         const urlParams = new URLSearchParams(window.location.search.toLowerCase());
         this.idPlastico = urlParams.get('?IdPlastico');
     }
 
+    public Plastico_nro: number;
     public plastico: number = 102409331;
     public infoPlastico: DetallePlasticoModel;
     ngOnInit() {
 
+       //console.log(  this.rutaActiva.snapshot.paramMap);
         this._DetallePlasticoService.GetInfoPlastico(this.plastico).subscribe(x => {
             this.infoPlastico = x.body;
            
